d606cff14f14566037aef25b76b00c4117d3e277
yurunull <kamomeyuru@gmail.com>
2025-05-01 17:30:52 +0900

    package.json変更

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: '/',
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-  };
+  },
 });

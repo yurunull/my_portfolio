f0a0ea9069770394f543c8371ccd7b2f0bd131f1
yurunull <kamomeyuru@gmail.com>
2025-05-01 17:37:38 +0900

    ぱっけーじじぇーそんへんこう

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/my_portfolio/',  // ここでサブディレクトリに合わせる
   plugins: [react()],
   resolve: {
     alias: {
